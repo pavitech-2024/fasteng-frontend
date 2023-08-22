e6e962def0dbce5e9bd1660a0b0a31088b8c8770
anagdinizp <anagabriela0399@hotmail.com>
2023-08-22 10:54:50 -0300

    feat: unit mass store

---
+++ src/stores/concrete/unitMass/unitMass.store.ts
@@ -0,0 +1,78 @@
+import { create } from 'zustand';
+import { devtools, persist, createJSONStorage } from 'zustand/middleware';
+
+interface UnitMassGeneralData {
+  userId: string;
+  name: string;
+  aggregate: unknown;
+  method: 'A' | 'B' | 'C';
+  materials: [];
+  materialSelected: object;
+}
+
+interface unitMass_step2Data {
+  containerVolume: string;
+  containerWeight: string;
+  sampleContainerWeight: string;
+}
+
+interface unitMass_step3Result {
+  result: string;
+}
+
+export type UnitMassData = {
+  generalData: UnitMassGeneralData;
+  step2Data: unitMass_step2Data;
+  result: unitMass_step3Result;
+};
+
+export type UnitMassActions = {
+  setData: ({ step, key, value }: setDataType) => void;
+};
+const stepVariant = { 0: 'generalData', 1: 'step2Data', 2: 'result' };
+
+export type setDataType = { step: number; key?: string; value: unknown };
+
+const useUnitMassStore = create<UnitMassData & UnitMassActions>()(
+  devtools(
+    persist(
+      (set) => ({
+        generalData: {
+          userId: null,
+          name: null,
+          aggregate: null,
+          method: null,
+          materials: null,
+          materialSelected: null,
+        },
+        step2Data: {
+          containerVolume: null,
+          containerWeight: null,
+          sampleContainerWeight: null,
+        },
+        result: {
+          result: null,
+        },
+
+        setData: ({ step, key, value }) =>
+          set((state) => {
+            if (key)
+              return {
+                ...state,
+                [stepVariant[step]]: {
+                  ...state[stepVariant[step]],
+                  [key]: value,
+                },
+              };
+            else return { ...state, [stepVariant[step]]: value };
+          }),
+      }),
+      {
+        name: 'unitMass-store',
+        storage: createJSONStorage(() => sessionStorage),
+      }
+    )
+  )
+);
+
+export default useUnitMassStore;
