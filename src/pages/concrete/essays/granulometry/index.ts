551adfb9dea144bab53516691698b10b1bb93cd3
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2023-09-11 20:18:58 -0300

    feature: soil compression essay finished

--- src/pages/concrete/essays/granulometry/index.ts
+++ src/pages/concrete/essays/granulometry/index.ts
@@ -2,34 +2,32 @@ import useAuth from '@/contexts/auth';
 import Granulometry_SERVICE from '@/services/concrete/essays/granulometry/granulometry.service';
 import useGranulometryStore, { GranulometryActions } from '@/stores/concrete/granulometry/granulometry.store';
 
-
 const ConcreteGranulometry = () => {
-    // start an instance of the service
-    const granulometry = new Granulometry_SERVICE();
-  
-    // get the userId
-    const {
-      user: { _id: userId },
-    } = useAuth();
-  
-    // get the store, could be empty or not ( in case of refresh page for example)
-    const store = useGranulometryStore();
-  
-    // set the userId to the service
-    granulometry.userId = userId;
-  
-    // set the store to the service
-    granulometry.store_actions = store as GranulometryActions;
-  
-    // inform the childrens with the step of the children and the part of the store that they will use
+  // start an instance of the service
+  const granulometry = new Granulometry_SERVICE();
+
+  // get the userId
+  const {
+    user: { _id: userId },
+  } = useAuth();
+
+  // get the store, could be empty or not ( in case of refresh page for example)
+  const store = useGranulometryStore();
+
+  // set the userId to the service
+  granulometry.userId = userId;
+
+  // set the store to the service
+  granulometry.store_actions = store as GranulometryActions;
+
+  // inform the childrens with the step of the children and the part of the store that they will use
   //   const childrens = [
   //   //  { step: 0, children: <ConcreteGranulometry_GeneralData granulometry={granulometry}/>, data: store.generalData },
   //  //   { step: 1, children: <ConcreteGranulometry_Step2 />, data: store },
   //   //  { step: 2, children: <ConcreteGranulometry_Results />, data: store },
   //   ];
-  
-   // return <EssayTemplate essayInfo={granulometry.info} nextCallback={granulometry.handleNext} childrens={childrens} />;
-  };
-  
-  export default ConcreteGranulometry;
-  
+
+  // return <EssayTemplate essayInfo={granulometry.info} nextCallback={granulometry.handleNext} childrens={childrens} />;
+};
+
+export default ConcreteGranulometry;
