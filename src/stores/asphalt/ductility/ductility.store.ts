a65b867bfb0a669c4e2f274e6444d64fd7ad46e8
Gabriel Guimaraẽs de Almeida <gabriel.almeida@ccc.ufcg.edu.br>
2023-10-03 17:01:23 -0300

    Formatação

--- src/stores/asphalt/ductility/ductility.store.ts
+++ src/stores/asphalt/ductility/ductility.store.ts
@@ -1,34 +1,34 @@
-import { AsphaltMaterial } from "@/interfaces/asphalt";
-import { create } from "zustand";
-import { createJSONStorage, devtools, persist } from "zustand/middleware";
+import { AsphaltMaterial } from '@/interfaces/asphalt';
+import { create } from 'zustand';
+import { createJSONStorage, devtools, persist } from 'zustand/middleware';
 
 interface GeneralData {
-    userId: string;
-    name: string;
-    material: AsphaltMaterial;
-    operator?: string;
-    calculist?: string;
-    description?: string;
+  userId: string;
+  name: string;
+  material: AsphaltMaterial;
+  operator?: string;
+  calculist?: string;
+  description?: string;
 }
 
 interface Ductility_step2Data {
-    first_rupture_length: number;
-    second_rupture_length: number;
-    third_rupture_length: number;
+  first_rupture_length: number;
+  second_rupture_length: number;
+  third_rupture_length: number;
 }
 
 interface Ductility_results {
-    ductility: number;
+  ductility: number;
 }
 
 export type DuctilityData = {
-    generalData: GeneralData;
-    step2Data: Ductility_step2Data;
-    results: Ductility_results;
+  generalData: GeneralData;
+  step2Data: Ductility_step2Data;
+  results: Ductility_results;
 };
 
 export type DuctilityActions = {
-    setData: ({ step, key, value }: setDataType) => void;
+  setData: ({ step, key, value }: setDataType) => void;
 };
 
 const stepVariant = { 0: 'generalData', 1: 'step2Data', 2: 'results' };
@@ -36,45 +36,45 @@ const stepVariant = { 0: 'generalData', 1: 'step2Data', 2: 'results' };
 type setDataType = { step: number; key?: string; value: unknown };
 
 const useDuctilityStore = create<DuctilityData & DuctilityActions>()(
-    devtools(
-        persist(
-            (set) => ({
-                generalData: {
-                    userId: null,
-                    name: null,
-                    material: null,
-                    operator: null,
-                    calculist: null,
-                    description: null,
-                },
-                step2Data: {
-                    first_rupture_length: null,
-                    second_rupture_length: null,
-                    third_rupture_length: null,
-                },
-                results: {
-                    ductility: null,
-                },
+  devtools(
+    persist(
+      (set) => ({
+        generalData: {
+          userId: null,
+          name: null,
+          material: null,
+          operator: null,
+          calculist: null,
+          description: null,
+        },
+        step2Data: {
+          first_rupture_length: null,
+          second_rupture_length: null,
+          third_rupture_length: null,
+        },
+        results: {
+          ductility: null,
+        },
 
-                setData: ({ step, key, value }) =>
-                    set((state) => {
-                        if (key)
-                            return {
-                                ...state,
-                                [stepVariant[step]]: {
-                                    ...state[stepVariant[step]],
-                                    [key]: value,
-                                },
-                            };
-                        else return { ...state, [stepVariant[step]]: value };
-                    }),
-            }),
-            {
-                name: 'ductility-store',
-                storage: createJSONStorage(() => sessionStorage),
-            }
-        )
+        setData: ({ step, key, value }) =>
+          set((state) => {
+            if (key)
+              return {
+                ...state,
+                [stepVariant[step]]: {
+                  ...state[stepVariant[step]],
+                  [key]: value,
+                },
+              };
+            else return { ...state, [stepVariant[step]]: value };
+          }),
+      }),
+      {
+        name: 'ductility-store',
+        storage: createJSONStorage(() => sessionStorage),
+      }
     )
+  )
 );
 
 export default useDuctilityStore;
