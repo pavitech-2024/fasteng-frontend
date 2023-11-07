fb0e8ed2d7a991ed057de7d9036e2d65e80d8815
Gabriel Guimaraẽs de Almeida <gabriel.almeida@ccc.ufcg.edu.br>
2023-11-07 12:46:28 -0300

    Checagem ok dos ensaios de Concreto e Solos

--- src/components/config/pages/navbar.tsx
+++ src/components/config/pages/navbar.tsx
@@ -172,11 +172,14 @@ export default function Navbar({ open, app }: NavbarProps) {
   ].filter((item) => item.app === Router.pathname.split('/')[1] || item.app === 'common');
 
   // when the user scroll the page, the navbar will be fixed on the top
-  window.addEventListener('scroll', () => {
-    if (window.innerWidth > 768)
-      if (window.scrollY === 0) document.getElementById('navbar').style.paddingTop = '52px';
-      else document.getElementById('navbar').style.paddingTop = '0';
-  });
+
+  // ISSO TÁ BUGADO 
+  
+  // window.addEventListener('scroll', () => {
+  //   if (window.innerWidth > 768)
+  //     if (window.scrollY === 0) document.getElementById('navbar').style.paddingTop = '52px';
+  //     else document.getElementById('navbar').style.paddingTop = '0';
+  // });
 
   return (
     <Box
