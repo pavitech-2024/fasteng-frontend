63bbe846252c71f2d411461ad365edc22f817829
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2023-11-20 20:04:40 -0300

    feat: sample visualization page initial implementation

--- src/services/soils/soils-samples.service.ts
+++ src/services/soils/soils-samples.service.ts
@@ -1,10 +1,16 @@
 import Api from '@/api';
 import { SampleData } from '@/interfaces/soils';
 
+// const samplesService = {
+//   createSample: (sampleData: SampleData) => Api.post('soils/samples', sampleData),
+//   deleteSample: (sampleId: string) => Api.delete(`soils/samples/${sampleId}`),
+//   getSamplesByUserId: (userId: string) => Api.get(`soils/samples/all/${userId}`),
+// };
+
 const samplesService = {
   createSample: (sampleData: SampleData) => Api.post('soils/samples', sampleData),
   deleteSample: (sampleId: string) => Api.delete(`soils/samples/${sampleId}`),
-  getSamplesByUserId: (userId: string) => Api.get(`soils/samples/all/${userId}`),
+  getSamplesByUserId: () => Api.get(`promedina/granular-layers/granular-layers-samples/all`),
 };
 
 export default samplesService;
