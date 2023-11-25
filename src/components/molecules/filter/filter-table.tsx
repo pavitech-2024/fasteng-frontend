d7f8fdb5cbf7e0f0cce5135b1235f78c1cd94f47
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2023-11-25 17:26:10 -0300

    feat: promedina view page implemented

--- src/components/molecules/filter/filter-table.tsx
+++ src/components/molecules/filter/filter-table.tsx
@@ -752,6 +752,64 @@ const PromedinaMaterialsTemplate = ({
                 </Box>
               </button>
             )}
+
+            {searchParams.highway !== '' && (
+              <button
+              onClick={() => setSearchParams({
+                ...searchParams,
+                highway: ''
+              })}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  border: '1px solid',
+                  borderColor: '#00A3FF',
+                  borderRadius: '15px',
+                  padding: '.1rem 1rem',
+                  backgroundColor: '#00A3FF',
+                }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '10px',
+                    borderRight: '1.5px solid',
+                    borderColor: 'primaryTons.white',
+                    pr: '1rem',
+                    width: '100%',
+                  }}
+                >
+                  <Typography variant="body1" sx={{ textAlign: 'start', color: '#FCFCFC' }}>
+                    Rodovia
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    height: '100%',
+                    alignItems: {
+                      mobile: 'start',
+                      notebook: 'center',
+                    },
+                    justifyContent: {
+                      mobile: 'end',
+                      notebook: 'center',
+                    },
+                  }}
+                >
+                  <CloseIcon
+                    sx={{
+                      fontSize: '2rem',
+                      color: 'primaryTons.white',
+                      cursor: 'pointer',
+                      pl: '1rem',
+                    }}
+                  />
+                </Box>
+              </button>
+            )}
           </Box>
         </Box>
 
