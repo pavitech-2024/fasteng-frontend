de781a50e0ee9626bcb7bf935bb6bb10ff76789f
pavitech <pavitechjbr@gmail.com>
2025-09-12 07:51:53 -0300

    ajuste de decimais nos inputs da granulometria ensaio

--- src/utils/hooks/asphalt/materials.hooks.tsx
+++ src/utils/hooks/asphalt/materials.hooks.tsx
@@ -58,7 +58,6 @@ export const useMaterials = (domain: 'asphalt' | 'soils' | 'concrete') => {
       if (domain === 'asphalt') {
         // asphalt retorna um objeto { materials, fwdEssays, iggEssays, rtcdEssays, dduiEssays }
 
-        console.log("Entrou no loadMaterials")
         const data = response.data[0];
         setMaterials(data.materials);
         setFwdEssays(data.fwdEssays);
@@ -128,7 +127,6 @@ export const useMaterials = (domain: 'asphalt' | 'soils' | 'concrete') => {
   const handleEditMaterial = useCallback(
     (materialId: string) => {
       try {
-        console.log("entrou no handleEditMaterial")
         const allMaterials = materials.concat(fwdEssays, iggEssays, rtcdEssays, dduiEssays);
         const selectedMaterial = allMaterials.find((m) => m._id === materialId) || null;
         setMaterialToEdit(selectedMaterial);
