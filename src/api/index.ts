af21c570234d76a7a9cd77e0b76531e55a404c6f
pavitech-2024 <pavitechfasteng@gmail.com>
2025-11-26 10:05:57 -0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,9 @@
 import Axios from 'axios';
 
-//.env
+//.env 'https://oyster-app-nekyt.ondigitalocean.app'
 const isLocalHost = process.env.NODE_ENV === 'development';
 
-const [localhost, test] = [
-  'http://localhost:8080',
-  // 'https://fasteng-backend-eocdo.ondigitalocean.app/'
-  'https://oyster-app-nekyt.ondigitalocean.app/',
-];
+const [localhost, test] = ['http://localhost:8080', 'https://fasteng-backend.vercel.app/'];
 
 const Api = Axios.create({
   baseURL: isLocalHost ? localhost : test,
