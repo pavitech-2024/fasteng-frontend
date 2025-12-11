8b1bc2f05dc24080fa4102685b0c3808a12f7908
pavitech-2024 <pavitechfasteng@gmail.com>
2025-12-11 17:55:41 -0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,10 @@
-import Axios from 'axios';
-
-//.env
 export const isLocalHost = process.env.NEXT_PUBLIC_ENV === 'development';
 export const isTest = process.env.NEXT_PUBLIC_ENV === 'test';
 
-const [localhost, prod] = [
+const [localhost, test, prod] = [
   'http://localhost:8080',
   'https://fasteng-backend.vercel.app/',
+  'https://fasteng-backend-ecodigta.ocean.app/',
 ];
 
 const Api = Axios.create({
@@ -16,5 +14,3 @@ const Api = Axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-export default Api;
