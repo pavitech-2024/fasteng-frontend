677be3043f4b0484bb63756075ce4b8a06c2ae6e
pavitech-2024 <pavitechfasteng@gmail.com>
2025-12-15 18:56:01 -0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,27 +1,30 @@
+import axios from 'axios';  
+
+// .env
 export const isLocalHost = process.env.NEXT_PUBLIC_ENV === 'development';
 export const isTest = process.env.NEXT_PUBLIC_ENV === 'test';
 
-
-//corrigi url
+// Corrigindo a URL (removendo barra extra no final)
 const [localhost, prod] = [
   'http://localhost:8080',
   'https://fasteng-backend.vercel.app',
 ];
 
-
 /*
-const [localhost, test, prod] = [
-  'http://localhost:8080',
-  'https://fasteng-backend.vercel.app/',
-  'https://fasteng-backend-ecodigta.ocean.app/',
-];
+  Caso precise de mais um ambiente (como o teste), adicione aqui:
+  const [localhost, test, prod] = [
+    'http://localhost:8080',
+    'https://fasteng-backend.vercel.app/',
+    'https://fasteng-backend-ecodigta.ocean.app/',
+  ];
 */
 
-
-const Api = Axios.create({
+const Api = axios.create({
   baseURL: isLocalHost || isTest ? localhost : prod,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
 });
+
+export default Api;
