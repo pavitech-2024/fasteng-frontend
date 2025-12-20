380c451435a559f79848dd71ce3eca40cfca4b85
“IagoSantos123” <is0941011@gmail.com>
2025-12-20 02:26:04 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuração para ignorar warnings do ESLint durante o build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  
-  // Opcional: também ignorar erros do TypeScript se necessário
-  typescript: {
-    ignoreBuildErrors: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
 
