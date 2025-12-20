9fd6d89bb14449861aa7de85d22d8b0699cefedf
“IagoSantos123” <is0941011@gmail.com>
2025-12-20 02:22:30 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -2,26 +2,12 @@
 const nextConfig = {
   // Configuração para ignorar warnings do ESLint durante o build
   eslint: {
-    // AVISO: Isto permite que o build passe mesmo com warnings/erros do ESLint
-    // Recomendado apenas para desenvolvimento ou para fazer o build temporariamente
     ignoreDuringBuilds: true,
   },
   
   // Opcional: também ignorar erros do TypeScript se necessário
   typescript: {
-    // IGNORA ERROS DE TYPESCRIPT DURANTE O BUILD (mais permissivo)
     ignoreBuildErrors: true,
-  },
-  
-  // Configuração existente do webpack para SVGs
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
   },
 };
 
