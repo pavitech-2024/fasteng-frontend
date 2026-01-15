4840c1de03f9729e752976e03268ff0845786eb2
pavitech-2024 <pavitechfasteng@gmail.com>
2026-01-15 03:14:54 -0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,7 @@ export const isProd = process.env.NEXT_PUBLIC_ENV === 'production';
 // URLs por ambiente
 const BASE_URL = isLocalHost
   ? 'http://localhost:8080'
-  : 'https://fasteng-backend.vercel.app';
+  : 'https://fasteng-backend-v2.vercel.app';
 
 const Api = Axios.create({
   baseURL: BASE_URL,
