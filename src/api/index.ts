ddb0822daac0036fc86f4a1bfcfab151aef88e52
Hyaguiin <137925587+Hyaguiin@users.noreply.github.com>
2026-04-16 16:35:43 -0300

    feat(modularizacao do binder-asphalt-concrete)

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import Axios from 'axios';
 //.env 'https://oyster-app-nekyt.ondigitalocean.app'
 const isLocalHost = process.env.NODE_ENV === 'development';
 
-const [localhost, test] = ['http://localhost:8080', 'https://fasteng-backend.vercel.app/'];
+const [localhost, test] = ['https://fasteng-backend.vercel.app/', 'https://fasteng-backend.vercel.app/'];
 
 const Api = Axios.create({
   baseURL: isLocalHost ? localhost : test,
